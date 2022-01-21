a6420054b3657b61751f6ad0e1e61079a1581ce2
sangwon lee <swlee0376@gmail.com>
2022-01-22 02:50:16 +0900

    amend style, add file
    default design fin

--- public/action/index.js
+++ public/action/index.js
@@ -1,4 +1,14 @@
-function onDisplay() {
-  // 속성 바꾸기
-  document.querySelector(".buttonPaging").style.display = "block";
+function showtab(a) {
+  for (i = 0; i < 4; i++) {
+    let target = document.getElementById("tab" + i);
+    let back = document.getElementsByClassName("buttonText" + i);
+
+    if (i == a) {
+      target.style.display = "block";
+      back.style.color = "red";
+    } else {
+      target.style.display = "none";
+      back.style.color = "blue";
+    }
+  }
 }
